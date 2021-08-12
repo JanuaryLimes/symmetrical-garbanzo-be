0c0ffffe620c6b418b15409a197d7c02253bd1da
Karol <qxtno@outlook.com>
2021-08-12 12:36:44 +0200

    Initialized application

---
+++ src/main/java/com/example/symmetricalgarbanzobe/SymmetricalGarbanzoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.symmetricalgarbanzobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricalGarbanzoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymmetricalGarbanzoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/symmetricalgarbanzobe/SymmetricalGarbanzoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.symmetricalgarbanzobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricalGarbanzoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
